17a1e26dc894e0093806d298f0317dcf2b736989
dijitald <drew.dials@outlook.com>
2022-04-09 20:22:27 -0400

    better drop down

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -5,7 +5,8 @@ import { Directive, ElementRef, HostBinding, HostListener, Input } from "@angula
 })
 export class DropDownDirective{
   @HostBinding('class.open') isOpen = false;
-  @HostListener('click') toggleOpen() {
-    this.isOpen = !this.isOpen;
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
   }
+  constructor(private elRef: ElementRef) {}
 }
